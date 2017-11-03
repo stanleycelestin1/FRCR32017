03151ac731036d8e6431567ee674c380fcacc862
Stanley Celestin <stanleycelestin1@gmail.com>
2017-11-03 16:06:15 -0500

    WE IN

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://stanley:celestin@ds243325.mlab.com:43325/blackhacks',
     options: {
       user: '',
       pass: ''
